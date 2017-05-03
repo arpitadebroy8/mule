2d967df2768b8ce587bd9f38a6812ee6a966c72a
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-05-03 19:20:58 -0300

    fixes

--- modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/operation/OperationMessageProcessorTestCase.java
+++ modules/extensions-support/src/test/java/org/mule/runtime/module/extension/internal/runtime/operation/OperationMessageProcessorTestCase.java
@@ -243,7 +243,8 @@ public class OperationMessageProcessorTestCase extends AbstractOperationMessageP
     when(context.getRegistry().lookupObject(OBJECT_EXPRESSION_LANGUAGE)).thenReturn(new MVELExpressionLanguage(context));
     when(context.getRegistry().lookupObject(DefaultExpressionLanguageFactoryService.class))
         .thenReturn(new WeaveDefaultExpressionLanguageFactoryService());
-    doReturn(new DefaultExpressionManager(context, new DefaultStreamingManager(muleContext))).when(context).getExpressionManager();
+    doReturn(new DefaultExpressionManager(context, new DefaultStreamingManager(muleContext))).when(context)
+        .getExpressionManager();
     FlowConstruct flowConstruct = mock(FlowConstruct.class);
     when(flowConstruct.getName()).thenReturn(flowName);
 
